6425e50bddafd8fb75b0858d56a462ea55c83d49
Phumrapee Limpianchop <git@rayriffy.com>
2020-10-29 23:50:30 +0900

    chore: intital c

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { NextPage } from 'next'
+
+const Page: NextPage = props => {
+  return (
+    <div className="p-6">
+      OK
+    </div>
+  )
+}
+
+export default Page
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      backgroundColor: {
+        'black-overlay': 'rgba(0, 0, 0, 0.60)',
+      },
+    },
+  },
+  variants: {},
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+  plugins: [require('@tailwindcss/ui')],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.tsx',
+    './src/**/*.ts',
+    './src/**/*.jsx',
+    './src/**/*.js',
+  ],
+}
