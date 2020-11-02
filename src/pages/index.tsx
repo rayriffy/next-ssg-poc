e24ace493f92e4272c0f45b39d7956dc0809122c
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-02 23:14:29 +0900

    chore: staged changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import { NextPage } from 'next'
 
 const Page: NextPage = props => {
   return (
-    <div className="p-6">
-      OK
+    <div className="p-6 prose">
+      Hello, go to <code>/poc-ssg/1</code> to begin
     </div>
   )
 }
